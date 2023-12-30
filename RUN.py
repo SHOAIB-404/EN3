e95c4df379df46c6b5bc6c04b78c25f82b988163
SHOAIB AHAMED "Sadi <142988274+SHOAIB-404@users.noreply.github.com>
2023-12-31 03:34:15 +0600

    Update RUN.py

--- RUN.py
+++ RUN.py
@@ -1,6 +1,5 @@
 import os, platform, time
-print('\n\x1b[1;33m Checking Update MONSTER...');time.sleep(0.5)
-os.system('git pull')
+print('\n\x1b[1;33m Checking Update THE_MONSTER 401...');time.sleep(0.5)
 os.system('xdg-open https://t.me/The_Monster_401')
 import RUN
 RUN.login()
