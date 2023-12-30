4a5caee148b2e9f66d241d714f25eec950a6b199
SHOAIB AHAMED "Sadi <142988274+SHOAIB-404@users.noreply.github.com>
2023-12-31 03:04:08 +0600

    Update RUN.py

--- RUN.py
+++ RUN.py
@@ -2,7 +2,9 @@ import os
  
 os.system('git pull')
  
-os.system('termux-setup-storage')
+os.system('xdg-open https://t.me/The_Monster_401')
+
+os.system('xdg-open https://t.me/Team_Chat_404')
  
 import compile
  
