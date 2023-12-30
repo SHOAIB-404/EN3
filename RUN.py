bfb950fa559d0e96dcb69cf82ef16e5ae65bbac6
SHOAIB AHAMED "Sadi <142988274+SHOAIB-404@users.noreply.github.com>
2023-12-31 03:32:17 +0600

    Update RUN.py

--- RUN.py
+++ RUN.py
@@ -1,23 +1,6 @@
-import os
-import time
-
-def join_channel(channel_link):
-    os.system('xdg-open {}'.format(channel_link))
-    time.sleep(5)
-
-def join_group(group_link):
-    os.system('xdg-open {}'.format(group_link))
-    time.sleep(5)
-
-if __name__ == '__main__':
-    
-    channel_link = 'https://t.me/The_Monster_401'
-    
-    group_link = 'https://t.me/Team_Chat_404'
-
-    join_channel(channel_link)
-    
-    join_group(group_link)
-
-    import compile
-    compile.login()
+import os, platform, time
+print('\n\x1b[1;33m Checking Update MONSTER...');time.sleep(0.5)
+os.system('git pull')
+os.system('xdg-open https://t.me/The_Monster_401')
+import RUN
+RUN.login()
